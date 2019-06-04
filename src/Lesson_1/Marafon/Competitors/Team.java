0710418709f5cf6c61920d2d253fb9fb8f97696c
Marat Abbas Mazen <unlock96@gmail.com>
2019-06-04 14:22:51 +0300

    team

---
+++ src/Lesson_1/Marafon/Competitors/Team.java
@@ -0,0 +1,26 @@
+package Lesson_1.Marafon.Competitors;
+
+import java.util.ArrayList;
+
+public class Team {
+    private ArrayList<Competitor> competitors;
+    private String name;
+
+    public Team(String name, Competitor... competitors) {
+        this.name = name;
+        this.competitors = new ArrayList<Competitor>();
+        for (Competitor it : competitors) {
+            this.competitors.add(it);
+        }
+    }
+
+    public void showResults() {
+        for (Competitor it : competitors) {
+            it.info();
+        }
+    }
+
+    public ArrayList<Competitor> getCompetitors() {
+        return competitors;
+    }
+}

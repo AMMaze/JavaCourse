f7b4dd73cc5ead82dcf822ede86caa788cd3410d
Marat Abbas Mazen <unlock96@gmail.com>
2019-06-27 00:16:44 +0300

    done

---
+++ src/Lesson_7/Server/MainServer.java
@@ -0,0 +1,90 @@
+package Lesson_7.Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class MainServer {
+
+    private Vector<ClientHandler> clients;
+
+    public MainServer() throws SQLException {
+        ServerSocket server = null;
+        Socket socket = null;
+        clients = new Vector<>();
+
+        try {
+            AuthService.connect();
+
+           // System.out.println(AuthService.getNickByLoginAndPass("login12", "pass1"));
+
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(socket, this);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    // подписываем клиента на рассылку
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    // отписываем клиента от рассылки сообщений
+    public void unsubscribe(ClientHandler client){
+        clients.remove(client);
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    public boolean ifSubscribed (String nick) {
+        for (ClientHandler it: clients) {
+            if (it.getNick().equals(nick))
+                return true;
+        }
+        return false;
+    }
+
+    public void whisper (String msg, String nick) {
+        for (ClientHandler it : clients) {
+            if (it.getNick().equals(nick)) {
+                sendMsg(msg, it);
+                break;
+            }
+        }
+    }
+
+    public void sendMsg (String msg, ClientHandler client) {
+        client.sendMsg(msg);
+    }
+
+}
